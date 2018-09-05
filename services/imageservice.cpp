3d42b9a3cf412c60d795965b77d560a16267b07d
Dmitry Gurov <dgurov@inbox.ru>
2018-09-05 17:42:32 +0300

    В гланом окне сверстаны строки вывода В сервисе изображений появилась дельта функция сравнения цвета

--- services/imageservice.cpp
+++ services/imageservice.cpp
@@ -40,6 +40,46 @@ QImage *ImageService::threshold(QImage *img, int val)
     return trImg;
 }
 
+bool ImageService::colorRange(QColor test, QColor etalon, int percentage)
+{
+    int eR = qRed(etalon.rgb());
+    int eG = qGreen(etalon.rgb());
+    int eB = qBlue(etalon.rgb());
+
+    double koeff = static_cast<double>(static_cast<double>(percentage) / static_cast<double>(100));
+
+    int eRMin = eR - static_cast<int>(eR*koeff);
+    eRMin = (eRMin >= 0) ? eRMin : 0;
+    int eGMin = eG - static_cast<int>(eG*koeff);
+    eGMin = (eGMin >= 0) ? eGMin : 0;
+    int eBMin = eB - static_cast<int>(eB*koeff);
+    eBMin = (eBMin >= 0) ? eBMin : 0;
+
+    int eRMax = eR + static_cast<int>(eR*koeff);
+    eRMax = (eRMax <= 255) ? eRMax : 255;
+    int eGMax = eG + static_cast<int>(eG*koeff);
+    eGMax = (eGMax <= 255) ? eGMax : 255;
+    int eBMax = eB + static_cast<int>(eB*koeff);
+    eBMax = (eBMax <= 255) ? eBMax : 255;
+
+    int tR = qRed(test.rgb());
+    int tG = qGreen(test.rgb());
+    int tB = qBlue(test.rgb());
+
+    bool cROk = false;
+    bool cGOk = false;
+    bool cBOk = false;
+
+    if(tR >= eRMin && tR <= eRMax)
+        cROk = true;
+    if(tG >= eGMin && tG <= eGMax)
+        cGOk = true;
+    if(tB >= eBMin && tB <= eBMax)
+        cBOk = true;
+
+    return cROk && cGOk && cBOk;
+}
+
 bool ImageService::borderArea(QImage img, int i, int j, int w)
 {
     bool top = false;
--- services/imageservice.h
+++ services/imageservice.h
@@ -12,9 +12,11 @@ public:
 
     static QPixmap addRect(QPixmap &pm, int w = 1, QColor clr = QColor(Qt::black));
     static QImage *threshold(QImage *img, int val);
+    static bool colorRange(QColor test, QColor etalon, int percentage);
 
 private:
     static bool borderArea(QImage img, int i, int j, int w = 1);
+
 };
 
 #endif // IMAGESERVICE_H
